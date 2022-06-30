6ba9c9514260aebc168be0b223953f4ff37eddd3
pedromartinrodriguez <pedromrodriguez@gmail.com>
2022-06-29 22:37:01 -0300

    Agregando funcionalidades 2da entrega proyecto

--- app.js
+++ app.js
@@ -1,185 +1,103 @@
 
-//alert("Calculador de nota final de alumnos ingresados");
+const productosContainer = document.querySelector("#contenedor-productos");
+const carritoContenedor = document.querySelector("#carrito-contenedor");
+const contadorCarrito = document.querySelector("#contadorCarrito");
+const precioTotal = document.querySelector("#precioTotal");
+const botonVaciarCarrito = document.querySelector("#vaciarCarrito");
 
-//Defino array de cursos
-const cursos = [{id: 1, nombre: "React", turno: "Noche", precio: 15000}, 
-    {id: 2, nombre: "Phyton", turno: "Tarde", precio: 12500},
-    {id: 1, nombre: "Javascript", turno: "Noche", precio: 8000},
-    {id: 1, nombre: "Base de datos", turno: "Tarde", precio: 25000}]; 
+let carrito = [];
 
-//Creando elemento p
-let tituloCursos = document.createElement("h2");
-tituloCursos.innerHTML = "<h2>Que cursos dictamos en Edu Code?</h2>";
 
-//Creando elementos desde objetos
-for (const curso of cursos) {
-    let container = document.createElement("div");
+cursos.forEach((curso) => {
+    const div = document.createElement('div');
+    div.classList.add('curso');
 
-    container.innerHTML = `
-        <ul>
-            <li>Curso: ${curso.nombre}</li>
-            <li>Turno: ${curso.turno}</li>
-            <li>Precio: ${curso.precio}</li>
-        </ul>`;
-    document.body.appendChild(container);
+    div.innerHTML = `
+                <img src="${curso.img}" alt="">
+                <h3>${curso.nombre}</h3>
+                <p>${curso.tipo}</p>
+                <p>${curso.turno}</p>
+                <p>Talle: ${curso.desc}</p>
+                <p class="precioProducto">Precio: $${curso.precio}</p>
+                <button onclick="agregarAlCarrito(${curso.id})" class="boton-agregar">Agregar <i class="fas fa-shopping-cart"></i></button>
+            `
+
+    productosContainer.append(div);
+
+});
+
+
+// FUNCION QUE ITERA SOBRE EL ARRAY, COMPARANDO SI ES IGUAL EL ID DE UN CURSO DEL ARRAY CON EL ID POR PARAMETRO
+const agregarAlCarrito = (id) => {
+    let item = cursos.find( (curso) => curso.id === id );
+    carrito.push(item);
+
+    console.log(carrito);
+    renderCarrito();
+    renderCantidadCarrito();
+    renderPrecioTotal();
+};
+
+const removerDelCarrito = (id) => {
+    const itemABorrar = carrito.find((curso) => curso.id === id);
+    const indice = carrito.indexOf(itemABorrar);
+    carrito.splice(indice, 1);
+
+    renderCarrito();
+    renderCantidadCarrito();
+    renderPrecioTotal();
+}
+
+const vaciarCarrito = () => {
+    carrito = []
+
+    renderCarrito();
+    renderCantidadCarrito();
+    renderPrecioTotal();
+}
+
+botonVaciarCarrito.addEventListener('click', vaciarCarrito)
+
+const renderCarrito = () => {
+    carritoContenedor.innerHTML = '';
+
+    carrito.forEach((item) => {
+        const div = document.createElement('div');
+        div.classList.add('productoEnCarrito');
+
+        div.innerHTML = `
+                        <p>${item.nombre}</p>
+                        <p>Precio: $${item.precio}</p>
+                        <button onclick="removerDelCarrito(${item.id})" class="boton-eliminar"><i class="fas fa-trash-alt"></i></button>
+        `
+        carritoContenedor.append(div);
+    })
+}
+
+// ACTUALIZAR EL NUMERO DEL CARRITO
+const renderCantidadCarrito = () => {
+    contadorCarrito.innerText = carrito.length;
+}
+
+const renderPrecioTotal = () => {
+    let total = 0;
+    carrito.forEach((curso) => {
+        total += curso.precio
+    })
+
+    precioTotal.innerText = total;
 }
 
 
 
 
-class Alumno {
-    constructor(nombre, curso, nota) {
-        this.nombre = nombre;
-        this.curso = curso;
-        this.nota = nota;
-    }
-}
-
-
-/*const alumno1 = new Alumno(
-    prompt("Ingrese el nombre del primer alumno"),
-    prompt("Ingrese su curso"),
-    Number(prompt("Ingrese su nota final"))
-);
-const alumno2 = new Alumno(
-    prompt("Ingrese el nombre del segundo alumno"),
-    prompt("Ingrese su curso"),
-    Number(prompt("Ingrese su nota final"))
-);
-const alumno3 = new Alumno(
-    prompt("Ingrese el nombre del tercer alumno"),
-    prompt("Ingrese su curso"),
-    Number(prompt("Ingrese su nota final"))
-);
-const alumno4 = new Alumno(
-    prompt("Ingrese el nombre del cuarto alumno"),
-    prompt("Ingrese su curso"),
-    Number(prompt("Ingrese su nota final"))
-);*/
-
-
-const alumnos = [];
-
-let form = document.getElementById("form");
-form.addEventListener("submit", validarFormulario);
-
-function validarFormulario(e){
-    e.preventDefault();
-    let form = e.target;
-    alumnos.push(form.children)
-    console.log(form.children[0]);
-    console.log(form.children[1]);
-    console.log(form.children[2]);
-    
-}
 
 
 
 
-console.log(alumnos);
-
-const notasAlumnos = [];
-
-console.log(notasAlumnos);
 
 
-/*if ((alumno1.nota < 0 || alumno2.nota < 0 || alumno3.nota < 0 || alumno4.nota < 0) || alumno1.nota > 10 || alumno2.nota > 10 || alumno3.nota > 10 || alumno4.nota > 10) {
 
-    alert("Recuerde que las notas deben ser estar entre el 0 y el 10")
 
-} else {
 
-    alert("Usted ingreso las notas correctamente")
 
-    function promedio(nota1, nota2, nota3, nota4) {
-
-        let notasSumadas = alumno1.nota + alumno2.nota + alumno3.nota + alumno4.nota;
-        let resultadoPromedio = notasSumadas / 4;
-        return resultadoPromedio;
-
-    }
-
-}*/
-
-for (let i = 0; i < notasAlumnos.length; i++) {
-    
-    alert("Las notas ingresadas son: " + notasAlumnos[i]);
-    
-}
-
-//Metodo para buscar una nota
-/*if (!notasAlumnos.includes(5) ) {
-
-    alert("Buscando por el metodo includes la nota 5. No hay una nota 5");
-
-} else {
-
-    alert("Buscando por el metodo includes la nota 5. Hay alumnos con nota 5");
-
-}
-
-//Metodo para filtrar notas aprobadas (mayores o iguales a 4)
-const notasAprobadas = notasAlumnos.filter(function(notas){
-
-    return notas >= 4;
-
-})
-
-if (notasAprobadas) {
-
-    alert("Las notas aprobadas son: " + notasAprobadas)
-
-} else {
-
-    alert("No hay alumnos aprobados")
-
-}
-
-//Arrojar la nota promedio del total
-//alert("La nota promedio es: " + promedio());
-*/
-
-// Agregando evento change al formulario
-
-const inputName = document.querySelector(".name");
-const inputCurso = document.querySelector(".curso");
-const inputNota = document.querySelector(".nota");
-
-inputName.addEventListener('change', () => {
-
-    if (inputName.value.length <= 5) {
-        alert("El nombre debe tener al menos 6 caracteres")
-        inputName.classList.add('error-formulario')
-        inputName.classList.remove('ok-formulario')
-    } else {
-        inputName.classList.add('ok-formulario')
-        inputName.classList.remove('error-formulario')
-    }
-
-})
-
-inputCurso.addEventListener('change', () => {
-
-    if(inputCurso.value.length <= 3) {
-        alert("El curso debe tener al menos 4 caracteres")
-        inputCurso.classList.add('error-formulario')
-        inputCurso.classList.remove('ok-formulario')
-    } else {
-        inputCurso.classList.add('ok-formulario')
-        inputCurso.classList.remove('error-formulario')
-    }
-
-})
-
-inputNota.addEventListener('change', () => {
-
-    if(inputNota.value <= 0 || inputNota.value > 10) {
-        alert("La nota debe estar entre 0 y 10")
-        inputNota.classList.add('error-formulario')
-        inputNota.classList.remove('ok-formulario')
-    } else {
-        inputNota.classList.add('ok-formulario')
-        inputNota.classList.remove('error-formulario')
-    }
-
-})
---
+++ profesores.js
@@ -0,0 +1,146 @@
+let alumnos = [
+    { id: 1, nombre: "Juan Gomez", curso: "Javascript I", nota: 8 },
+    { id: 2, nombre: "Maria Fernandez", curso: "Javascript II", nota: 2 },
+    { id: 3, nombre: "Ines Perez", curso: "Base de Datos I", nota: 4 },
+    { id: 4, nombre: "Marcos Landajo", curso: "Base de Datos II", nota: 9 },
+    { id: 5, nombre: "Andrea Gomez", curso: "Javascript I", nota: 3 }
+];
+
+
+// Agregando evento change al formulario
+
+const inputName = document.getElementById("name");
+const inputCurso = document.getElementById("curso");
+const inputNota = document.getElementById("nota");
+
+inputName.addEventListener('change', () => {
+
+    if (inputName.value.length <= 5) {
+        alert("El nombre debe tener al menos 6 caracteres")
+        inputName.classList.add('error-formulario')
+        inputName.classList.remove('ok-formulario')
+    } else {
+        inputName.classList.add('ok-formulario')
+        inputName.classList.remove('error-formulario')
+    }
+
+})
+
+inputCurso.addEventListener('change', () => {
+
+    if (inputCurso.value.length <= 3) {
+        alert("El curso debe tener al menos 4 caracteres")
+        inputCurso.classList.add('error-formulario')
+        inputCurso.classList.remove('ok-formulario')
+    } else {
+        inputCurso.classList.add('ok-formulario')
+        inputCurso.classList.remove('error-formulario')
+    }
+
+})
+
+inputNota.addEventListener('change', () => {
+
+    if (inputNota.value <= 0 || inputNota.value > 10) {
+        alert("La nota debe estar entre 0 y 10")
+        inputNota.classList.add('error-formulario')
+        inputNota.classList.remove('ok-formulario')
+    } else {
+        inputNota.classList.add('ok-formulario')
+        inputNota.classList.remove('error-formulario')
+    }
+
+})
+
+
+let form = document.getElementById("form");
+form.addEventListener("submit", validarFormulario);
+
+function validarFormulario(e) {
+    e.preventDefault();
+    let form = e.target;
+    alumnos.push(form.children)
+    console.log(form.children[0]);
+    console.log(form.children[1]);
+    console.log(form.children[2]);
+
+}
+
+//console.log(alumnos);
+
+
+const alumnosContainer = document.querySelector("#contenedor-alumnos");
+
+alumnos.forEach((alumno) => {
+    const div = document.createElement('div');
+    div.classList.add('alumno');
+
+    div.innerHTML = `
+                    <h3>Nombre Alumno: ${alumno.nombre}</h3>
+                    <p>Curso: ${alumno.curso}</p>
+                    <p>Nota: ${alumno.nota}</p>
+    `
+    alumnosContainer.append(div);
+})
+
+function obtenerDatos() {
+    let nuevoAlumno = {
+        id: alumnos.length++,
+        nombre: document.getElementById('name').value,
+        curso: document.getElementById('curso').value,
+        nota: document.getElementById('nota').value
+    };
+
+    if (nuevoAlumno) {
+    const div = document.createElement('div');
+    div.classList.add('alumno');
+
+    div.innerHTML = `
+                    <h3>Nombre Alumno: ${nuevoAlumno.nombre}</h3>
+                    <p>Curso: ${nuevoAlumno.curso}</p>
+                    <p>Nota: ${nuevoAlumno.nota}</p>
+    `
+    alumnosContainer.append(div);
+}
+
+    alumnos.push(nuevoAlumno);
+    
+}
+
+// CALCULADOR DE NOTA PROMEDIO
+let notasAlumnos = [];
+
+alumnos.forEach((num) => {
+    notasAlumnos.push(num.nota)
+})
+console.log(notasAlumnos);
+
+function promedio(notasAlumnos) {
+    let i = 0, summ = 0, ArrayLen = notasAlumnos.length;
+    while (i < ArrayLen) {
+        summ = summ + notasAlumnos[i++];
+}
+    return summ / ArrayLen;
+}
+
+let a = promedio(notasAlumnos);
+console.log(a)
+
+
+// CALCULADOR NOTA PROMEDIO EN DOM
+const notaPromedio = document.querySelector("#nota-promedio");
+
+const div = document.createElement('div');
+    div.classList.add('nota');
+
+    div.innerHTML = `
+                    <h3>La nota promedio es: ${a}</h3>           
+    `
+    alumnosContainer.append(div);
+
+// ALMACENAMIENTO DE ARRAY ALUMNOS EN LOCAL STORAGE
+
+const alumnosEnJSON = JSON.stringify(alumnos);
+console.log(alumnosEnJSON);
+localStorage.setItem("alumnos", alumnosEnJSON);
+
